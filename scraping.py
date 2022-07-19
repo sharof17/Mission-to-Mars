bf65dd752dfd466efbb3d29f5e7c478c5ef4802c
Sharof <abd.sharof@gmail.com>
2022-07-19 18:19:34 -0500

    Adding the final draft

--- scraping.py
+++ scraping.py
@@ -19,6 +19,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        "hemispheres": hemispheres(browser),
         "last_modified": dt.datetime.now()
     }
 
@@ -97,6 +98,37 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
+# Challenge
+def hemispheres(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    # Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # Write code to retrieve the image urls and titles for each hemisphere.
+    links = browser.find_by_css('a.product-item h3')
+
+    # Loop through those links click the link find the sample anchor return href
+    for index in range(4):
+        hemisphere = {}
+    
+        # Find the elements on each loop to avoid a state element exception
+        browser.find_by_css('a.product-item h3')[index].click()
+
+        # Find the sample image anchor tag and extract href
+        sample_element = browser.links.find_by_text("Sample").first
+        hemisphere["img_url"] = sample_element["href"]
+    
+        # Get Hemisphere title
+        hemisphere["title"] = browser.find_by_css("h2.title").text
+        
+        hemisphere_image_urls.append(hemisphere)
+        
+        browser.back()
+        
+    return hemisphere_image_urls
+    
 if __name__ == "__main__":
 
     # If running as script, print scraped data
